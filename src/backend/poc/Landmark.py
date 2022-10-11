86f42e1a264015f02b3be46f2ef666756b135167
Bill Song <songchao456@gmail.com>
2022-10-11 10:23:09 -0400

    add sample images and drivers

--- src/backend/poc/Landmark.py
+++ src/backend/poc/Landmark.py
@@ -3,55 +3,92 @@ import cv2
 import mediapipe as mp
 
 # INITIALIZING OBJECTS
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_face_mesh = mp.solutions.face_mesh
+MP_DRAWING = mp.solutions.drawing_utils
+MP_DRAWING_STYLE = mp.solutions.drawing_styles
+MP_FACE_MESH = mp.solutions.face_mesh
+FACE_MESH_ENGINE = MP_FACE_MESH.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+DRAWING_SPEC = MP_DRAWING.DrawingSpec(thickness=1, circle_radius=1)
 
-drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-cap = cv2.VideoCapture(0)
 
-# DETECT THE FACE LANDMARKS
-with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5) as face_mesh:
+def read_face_mesh(img):
+    # DETECT THE FACE LANDMARKS
+
+    # Flip the image horizontally and convert the color space from BGR to RGB
+    img = cv2.cvtColor(cv2.flip(img, 1), cv2.COLOR_BGR2RGB)
+
+    # To improve performance
+    img.flags.writeable = False
+
+    # Detect the face landmarks
+    results = FACE_MESH_ENGINE.process(img)
+
+    img.flags.writeable = True
+
+    # Convert back to the BGR color space
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    return img, results
+
+
+# Sample driver code for image input
+def input_image(img=None, img_path=None):
+    if not img and not img_path:
+        return None
+
+    if not img:
+        img = cv2.imread(img_path)
+    img, landmarks = read_face_mesh(img)
+    # cv2.imshow('MediaPipe FaceMesh', img)
+
+    if landmarks.multi_face_landmarks:
+        # To improve performance
+        for face_landmarks in landmarks.multi_face_landmarks:
+            #   Showing the landmark coordinates
+            # for id,lm in enumerate(face_landmarks.landmark):
+            #     ih, iw, ic = image.shape
+            #     x,y = int(lm.x*iw), int(lm.y*ih)
+            #     # Prints the x, y coordinates
+            #     print(id, x,y)
+            MP_DRAWING.draw_landmarks(
+                image=img,
+                landmark_list=face_landmarks,
+                connections=MP_FACE_MESH.FACEMESH_TESSELATION,
+                landmark_drawing_spec=None,
+                connection_drawing_spec=MP_DRAWING_STYLE.get_default_face_mesh_tesselation_style())
+            # Display the image
+
+    cv2.imshow('MediaPipe FaceMesh', img)
+    cv2.waitKey(0)
+    return img
+
+
+# Sample driver code for live streaming
+def input_videostream():
+    cap = cv2.VideoCapture(0)
     while True:
-        success, image = cap.read()
-
-        # Flip the image horizontally and convert the color space from BGR to RGB
-        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-
-        # To improve performance
-        image.flags.writeable = False
-
-        # Detect the face landmarks
-        results = face_mesh.process(image)
-
-        # To improve performance
-        image.flags.writeable = True
-
-        # Convert back to the BGR color space
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        # Draw the face mesh annotations on the image.
-        if results.multi_face_landmarks:
-            for face_landmarks in results.multi_face_landmarks:
+        success, img = cap.read()
+        img, landmarks = read_face_mesh(img)
+        if landmarks.multi_face_landmarks:
+            # To improve performance
+            for face_landmarks in landmarks.multi_face_landmarks:
                 #   Showing the landmark coordinates
                 # for id,lm in enumerate(face_landmarks.landmark):
                 #     ih, iw, ic = image.shape
                 #     x,y = int(lm.x*iw), int(lm.y*ih)
                 #     # Prints the x, y coordinates
                 #     print(id, x,y)
-                mp_drawing.draw_landmarks(
-                    image=image,
+                MP_DRAWING.draw_landmarks(
+                    image=img,
                     landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_TESSELATION,
+                    connections=MP_FACE_MESH.FACEMESH_TESSELATION,
                     landmark_drawing_spec=None,
-                    connection_drawing_spec=mp_drawing_styles
-                        .get_default_face_mesh_tesselation_style())
+                    connection_drawing_spec=MP_DRAWING_STYLE.get_default_face_mesh_tesselation_style())
+                # Display the image
+                cv2.imshow('MediaPipe FaceMesh', img)
+                if cv2.waitKey(5) & 0xFF == 27:
+                    break
+    cap.release()
 
-        # Display the image
-        cv2.imshow('MediaPipe FaceMesh', image)
 
-        # Terminate the process
-        if cv2.waitKey(5) & 0xFF == 27:
-            break
-
-cap.release()
+if __name__ == '__main__':
+    img = input_image(img_path='./images/test2.jpeg')
+    # input_videostream()
