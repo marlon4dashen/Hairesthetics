882af7a18d5d5853dadb2e6cfdbf574cff6c3804
Manyi Cheng <chengm33@mcmaster.ca>
2022-11-19 17:56:24 -0500

    added menu scrollview

---
+++ src/Hairesthetics/Hairesthetics/ui/Box.swift
@@ -0,0 +1,20 @@
+//
+//  Box.swift
+//  Hairesthetics
+//
+//  Created by Charlotte Cheng on 2022-11-19.
+//
+
+import SwiftUI
+
+struct Box: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Box_Previews: PreviewProvider {
+    static var previews: some View {
+        Box()
+    }
+}
