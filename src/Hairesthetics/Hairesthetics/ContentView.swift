a9d23be03b07e9c657bdac23c1def7f6e228a80e
Manyi Cheng <chengm33@mcmaster.ca>
2022-11-19 17:58:23 -0500

    added menu scrollview

--- src/Hairesthetics/Hairesthetics/ContentView.swift
+++ src/Hairesthetics/Hairesthetics/ContentView.swift
@@ -8,16 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedBox: String? = nil
+    let hairstyles: [Box] = [
+        Box(title:"Long", imageUrl:"hairstyle0"),
+        Box(title:"Medium", imageUrl:"hairstyle1"),
+        Box(title:"Short", imageUrl:"hairstyle2"),
+        Box(title:"Long1", imageUrl:"hairstyle0"),
+        Box(title:"Medium1", imageUrl:"hairstyle1"),
+        Box(title:"Short1", imageUrl:"hairstyle2")
+    ]
     var body: some View {
         NavigationView{
-            Text("Hellow Word")
-                .navigationBarTitle(Text("Hairesthetics"))
+            ScrollView{
+                HStack{
+                    ForEach(hairstyles) { hair in
+                        BoxView(selectedBox: self.$selectedBox, box: hair)
+                    }
+                }
+            }
+            .padding(5)
+            
         }
     }
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
+#endif
--- src/Hairesthetics/Hairesthetics/ui/Box.swift
+++ src/Hairesthetics/Hairesthetics/ui/Box.swift
@@ -7,14 +7,32 @@
 
 import SwiftUI
 
-struct Box: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Box : Identifiable {
+    var id = UUID()
+    let title, imageUrl: String
+    
+}
+
+struct BoxView: View{
+    @Binding var selectedBox: String?
+    
+    let box: Box
+    var body: some View{
+        VStack{
+            Image(box.imageUrl)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 80, height: 80)
+                .clipped()
+                .cornerRadius(12)
+                .opacity(self.box.title == selectedBox ? 0.5: 1)
+                .foregroundColor(self.box.title == selectedBox ? Color.gray : Color.white)
+            Text(box.title)
+                .font(.subheadline)
+                .fontWeight(.semibold)
+        }.onTapGesture {
+            self.selectedBox = self.box.title
+        }
     }
 }
 
-struct Box_Previews: PreviewProvider {
-    static var previews: some View {
-        Box()
-    }
-}
