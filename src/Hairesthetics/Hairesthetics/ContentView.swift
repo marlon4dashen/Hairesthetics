4692052acf99768917ccee11eab69d61eb08b891
Manyi Cheng <chengm33@mcmaster.ca>
2022-11-23 19:24:24 -0500

    merged menu and settings and camera

--- src/Hairesthetics/Hairesthetics/ContentView.swift
+++ src/Hairesthetics/Hairesthetics/ContentView.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 import AVFoundation
+import RealityKit
+import ARKit
+
 class AppSettings: ObservableObject {
     @Published var currentMode: String? = "Haircolor"
     @Published var selectedHair: String? = nil
@@ -15,12 +18,35 @@ class AppSettings: ObservableObject {
 }
 
 struct ContentView: View {
+    @State private var isPlacementEnabled = false
+    @State private var selectedModel: String?
+    @State private var modelConfirmed: String?
     @StateObject var settings = AppSettings()
+    private var models: [String] = {
+            let filemanager = FileManager.default
+            guard let path = Bundle.main.resourcePath, let files = try?filemanager.contentsOfDirectory(atPath: path) else {
+                return []
+            }
+            var availableodels: [String] = []
+            for filename in files where filename.hasSuffix("usdz"){
+                let modelName = filename.replacingOccurrences(of: ".usdz", with: "")
+                availableodels.append(modelName)
+            }
+            return availableodels
+        }()
     var body: some View {
         NavigationView {
             VStack(spacing: 0){
                 // CameraView() # comment to run on simulator
-
+                ZStack(alignment: .bottom){
+                    ARViewContainer(modelConfirmed: $modelConfirmed)
+                    if self.isPlacementEnabled{
+                        placeButtons(isPlacementEnabled: self.$isPlacementEnabled, selectedModel: self.$selectedModel, modelConfirmed: self.$modelConfirmed)
+                    }else{
+                        ModelPickerView(isPlacementEnabled: self.$isPlacementEnabled, selectedModel: self.$selectedModel,models: self.models)
+                    }
+                            
+                }
                 if settings.currentMode == "Hairstyle" {
                     HairstyleMenu().environmentObject(settings)
                 }else{
@@ -42,7 +68,87 @@ struct ContentView: View {
 }
 
 
-
+struct ARViewContainer: UIViewRepresentable {
+    @Binding var modelConfirmed: String?
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        config.environmentTexturing = .automatic
+        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh){
+            config.sceneReconstruction = .mesh
+        }
+        arView.session.run(config)
+    
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {
+        if let modelName = self.modelConfirmed{
+            print("Debug3 \(modelName)")
+            let filename = modelName+".usdz"
+            let modelEntity = try! ModelEntity.loadModel(named: filename)
+            let anchorEntity = AnchorEntity()
+            anchorEntity.addChild(modelEntity)
+            uiView.scene.addAnchor(anchorEntity)
+            
+            DispatchQueue.main.async {
+                self.modelConfirmed = nil
+            }
+        }
+    }
+    
+}
+struct placeButtons: View{
+    @Binding var isPlacementEnabled: Bool
+    @Binding var selectedModel: String?
+    @Binding var modelConfirmed: String?
+    var body: some View{
+        HStack {
+            Button(action:{
+                print("Debug1")
+                self.isPlacementEnabled = false
+                self.selectedModel = nil
+            }){
+                Image(systemName: "xmark").frame(width: 40, height: 40).font(.title).background(Color.white.opacity(0.75)).cornerRadius(20).padding(0)
+            }
+            Button(action:{
+                print("Debug2")
+                self.modelConfirmed = self.selectedModel
+                self.isPlacementEnabled = false
+                self.selectedModel = nil
+            }){
+                Image(systemName: "checkmark").frame(width: 40, height: 40).font(.title).background(Color.white.opacity(0.75)).cornerRadius(20).padding(0)
+            }
+        }
+    }
+}
+struct ModelPickerView: View{
+    @Binding var isPlacementEnabled: Bool
+    @Binding var selectedModel: String?
+    var models: [String]
+    var body: some View{
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 30){
+                ForEach(0..<self.models.count){
+                    index in
+                    Button(action: {
+                        print("Debug: \(self.models[index])")
+                        self.selectedModel = self.models[index]
+                        self.isPlacementEnabled = true
+                        
+                    }) {
+                        Image(uiImage: UIImage(named: self.models[index])!).resizable().frame(height:80).aspectRatio(1/1, contentMode: .fit).background(Color.white).cornerRadius(12)
+                    }.buttonStyle(PlainButtonStyle())
+                }
+            }
+        }
+        .padding(20).background(Color.black.opacity(0.5))
+    }
+}
 
 struct CameraView: View {
 
