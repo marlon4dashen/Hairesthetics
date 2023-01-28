3b23baf4e2322ea7fbbf4ac8ca613608be44d7b5
Bill Song <songchao456@gmail.com>
2023-01-28 12:06:15 -0500

    add header

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -5,19 +5,21 @@ import HairStyleView from "./pages/HairStyleView"
 import HairColorView from "./pages/HairColorView"
 import FaceView from "./pages/FaceView"
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-          <Switch>
-            <Route path="/" component={HomeView} exact={true}/>
-            <Route path="/face" component={FaceView}/>
-            <Route path="/style" component={HairStyleView}/>
-            <Route path="/color" component={HairColorView}/>
-          </Switch>
+        <Header />
+        <Switch>
+          <Route path="/" component={HomeView} exact={true}/>
+          <Route path="/face" component={FaceView}/>
+          <Route path="/style" component={HairStyleView}/>
+          <Route path="/color" component={HairColorView}/>
+        </Switch>
       </BrowserRouter>
-      </div>
+    </div>
   );
 }
 
