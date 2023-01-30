bb4e501760cad88fe7f3595539b87d83137fd2d4
Bill Song <songchao456@gmail.com>
2023-01-29 21:50:32 -0500

    add salon markers and info window on the map

--- src/frontend/src/pages/SalonRecommendationView.js
+++ src/frontend/src/pages/SalonRecommendationView.js
@@ -1,42 +1,144 @@
-import React, { useEffect, useState } from "react"
-// import Map from "../components/Map";
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import React, { useEffect, useState, useMemo } from "react"
 import {Container, Button, Row, Col, Card, ListGroup, Form, InputGroup} from 'react-bootstrap'
 import {FaSearch} from 'react-icons/fa';
 import axios from 'axios';
+import '../css/SalonRecommendation.css';
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
 
-const containerStyle = {
-  width: '900px',
-  height: '500px'
-};
-
+const { GOOGLE_MAPS_API_KEY } = require("../config.json");
 function SalonRecommendationView() {
     const { isLoaded } = useLoadScript({
-        // id: 'google-map-script',
-        googleMapsApiKey: "",
-    })
-    const [map, setMap] = useState(null)
+        googleMapsApiKey: GOOGLE_MAPS_API_KEY,
+        libraries: ["places"],
+    });
+    
     const [geolocation, setGeolocation] = useState (null)
     const [lat, setLat] = useState(null)
     const [lng, setLng] = useState(null)
-    const [geoError, setGeoError] = useState(null)
     const [resultsLength, setResultLength] = useState(0)
     const [searchResults, setSearchResults] = useState([])
     const [inputAddress, setAddress] = useState("")
 
-    const onLoad = React.useCallback(function callback(map) {
-        // This is just an example of getting and using the map instance!!! don't just blindly copy!
-        // const bounds = new window.google.maps.LatLngBounds(center);
-        // console.log(lat);
-        // <Marker position={{ lat: lat, lng: lng }} icon={{url: require('./placeholder.png')}}/>
-        // map.fitBounds(bounds);
+    function Map() {
+        const center = useMemo(() => ({ lat: 43.45, lng: -80.49 }), []);
+        const [selected, setSelected] = useState(null);
+        const [selectedPlace, setSelectedPlace] = useState(null)
+        useEffect(() => {
+            const listener = e => {
+            if (e.key === "Escape") {
+                setSelectedPlace(null);
+            }
+            };
+            window.addEventListener("keydown", listener);
 
-        setMap(map)
-    }, [])
+            return () => {
+                window.removeEventListener("keydown", listener);
+            };
+        }, []);
+        return (
+            <>
+            <div >
+                <PlacesAutocomplete setSelected={setSelected} />
+            </div>
+         
+            <GoogleMap
+                zoom={10}
+                center={center}
+                mapContainerClassName="map-container"
+            >
+                {/* {selected && <Marker position={selected} />} */}
+                {searchResults.length > 0 && 
+                searchResults.map((salon)=>(
+                    <Marker 
+                        key={salon.place_id} 
+                        position={{lat: salon.lat, lng:salon.lng}}
+                        onClick={() => {
+                            setSelectedPlace(salon);
+                        }}
+                    />
+                ))}
+                {selectedPlace && (
+                <InfoWindow
+                    // info window open at clicked location
+                    position={{lat: selectedPlace.lat, lng: selectedPlace.lng}}
+                    // after clicking the close button
+                    onCloseClick={() => {
+                        setSelectedPlace(null);
+                    }}
+                >
+                    {/* Display location information */}
+                    <div className="infoWindow" style={{fontWeight: 'bold', color: 'blue'}}>
+                        <p>{selectedPlace.name}</p>
+                        {/* <p>Capacity: {selectedPlace.BICYCLE_CAPACITY}</p>
+                        {props.showLink && (
+                            <p><a href={`./single/id=${selectedPlace.id}`}>More Details</a></p>
+                        )} */}
+                    </div>
+                </InfoWindow>
+                )}
+            </GoogleMap>
+            </>
+        );
+    }
 
-    const onUnmount = React.useCallback(function callback(map) {
-        setMap(null)
-    }, [])
+    const PlacesAutocomplete = ({ setSelected }) => {
+        const {
+            ready,
+            value,
+            setValue,
+            suggestions: { status, data },
+            clearSuggestions,
+        } = usePlacesAutocomplete();
+
+        const handleSelect = async (address) => {
+            setValue(address, false);
+            clearSuggestions();
+
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            setSelected({ lat, lng });
+        };
+
+        return (
+            <>
+                <InputGroup className="my-3">
+                    <Combobox onSelect={handleSelect}>
+                <ComboboxInput
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                    disabled={!ready}
+                    className="combobox-input"
+                    placeholder="Search an address"
+                />
+                <ComboboxPopover>
+                    <ComboboxList>
+                    {status === "OK" &&
+                        data.map(({ place_id, description }) => (
+                        <ComboboxOption key={place_id} value={description} />
+                        ))}
+                    </ComboboxList>
+                </ComboboxPopover>
+            </Combobox>
+                    {/* <Button variant="outline-primary" onClick={searchNearbySalon}>
+                        <FaSearch />
+                    </Button> */}
+                    <Button variant="outline-info" onClick={searchNearbySalon}>Use current location</Button>
+                </InputGroup>
+            </>
+        );
+    };
 
     const searchNearbySalon = () => {
         axios.get(`http://localhost:5001/salons?lat=${lat}&lng=${lng}`)
@@ -65,7 +167,7 @@ function SalonRecommendationView() {
                 setAddress(`${position.coords.latitude}, ${position.coords.longitude}`)
             },
             function(error) {
-                setGeoError(error);
+                // setGeoError(error);
                 console.error("Error Code = " + error.code + " - " + error.message);
             }
         );
@@ -76,20 +178,8 @@ function SalonRecommendationView() {
 
     return (
         <>
-            {/* {(lat & lng) ? (
-                <GoogleMap
-                    
-                    mapContainerStyle={containerStyle}
-                    center={{lat:lat, lng:lng}}
-                    zoom={10}
-                    // onLoad={onLoad}
-                    onUnmount={onUnmount}
-                >
-                {geolocation && <Marker position={{ lat: lat, lng: lng }} icon={{url: require('./placeholder.png'), scaledSize: new window.google.maps.Size(37, 37)}}/>}
-                
-                </GoogleMap>
-            ) : <div>Loading...</div>} */}
-            <Container>
+        {/* <MapGL className="map-container" /> */}
+            {/* <Container>
                 <InputGroup className="my-3">
                     <Form.Control
                         placeholder="Enter a street address"
@@ -102,9 +192,11 @@ function SalonRecommendationView() {
                     </Button>
                     <Button variant="outline-info" onClick={searchNearbySalon}>Use current location</Button>
                 </InputGroup>
+            </Container> */}
+            <Container>
+            {/* <Places /> */}
+            {isLoaded && <Map />}
             </Container>
-            {/* {geolocation ? (<><p>{geolocation.latitude.toFixed(4)}</p>
-            <p>{geolocation.longitude.toFixed(4)}</p></>):(<p>Not found</p>)} */}
             <Container fluid>
                 <p>{resultsLength} results found</p>
                 {( resultsLength >0 ) ? (
