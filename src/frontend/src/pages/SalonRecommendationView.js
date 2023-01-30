671d95e0bc6e1488a0699627e2e6e07a6e00c25f
Bill Song <songchao456@gmail.com>
2023-01-30 11:29:36 -0500

    add hooks for the search bar

--- src/frontend/src/pages/SalonRecommendationView.js
+++ src/frontend/src/pages/SalonRecommendationView.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useMemo } from "react"
-import {Container, Button, Row, Col, Card, ListGroup, Form, InputGroup} from 'react-bootstrap'
-import {FaSearch} from 'react-icons/fa';
+import {Container, Button, Row, Col, Card, ListGroup} from 'react-bootstrap'
+import {IoMdLocate} from 'react-icons/io';
 import axios from 'axios';
 import '../css/SalonRecommendation.css';
 import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
@@ -24,16 +24,14 @@ function SalonRecommendationView() {
         libraries: ["places"],
     });
     
-    const [geolocation, setGeolocation] = useState (null)
-    const [lat, setLat] = useState(null)
-    const [lng, setLng] = useState(null)
     const [resultsLength, setResultLength] = useState(0)
     const [searchResults, setSearchResults] = useState([])
     const [inputAddress, setAddress] = useState("")
+    const [selected, setSelected] = useState(null);
 
     function Map() {
-        const center = useMemo(() => ({ lat: 43.45, lng: -80.49 }), []);
-        const [selected, setSelected] = useState(null);
+        const center = useMemo(() => ({ lat: 43.6532, lng: -79.3832 }), []);
+        
         const [selectedPlace, setSelectedPlace] = useState(null)
         useEffect(() => {
             const listener = e => {
@@ -49,13 +47,9 @@ function SalonRecommendationView() {
         }, []);
         return (
             <>
-            <div >
-                <PlacesAutocomplete setSelected={setSelected} />
-            </div>
-         
             <GoogleMap
                 zoom={10}
-                center={center}
+                center={(selected) ? selected : center}
                 mapContainerClassName="map-container"
             >
                 {/* {selected && <Marker position={selected} />} */}
@@ -64,6 +58,7 @@ function SalonRecommendationView() {
                     <Marker 
                         key={salon.place_id} 
                         position={{lat: salon.lat, lng:salon.lng}}
+                        icon="./assets/icons/seat.png"
                         onClick={() => {
                             setSelectedPlace(salon);
                         }}
@@ -73,18 +68,15 @@ function SalonRecommendationView() {
                 <InfoWindow
                     // info window open at clicked location
                     position={{lat: selectedPlace.lat, lng: selectedPlace.lng}}
-                    // after clicking the close button
                     onCloseClick={() => {
                         setSelectedPlace(null);
                     }}
                 >
                     {/* Display location information */}
                     <div className="infoWindow" style={{fontWeight: 'bold', color: 'blue'}}>
-                        <p>{selectedPlace.name}</p>
-                        {/* <p>Capacity: {selectedPlace.BICYCLE_CAPACITY}</p>
-                        {props.showLink && (
-                            <p><a href={`./single/id=${selectedPlace.id}`}>More Details</a></p>
-                        )} */}
+                        <h4>{selectedPlace.name}</h4>
+                        <p>Ratings: {selectedPlace.rating}</p>
+                        <p>Total Reviews: {selectedPlace.user_ratings_total}</p>
                     </div>
                 </InfoWindow>
                 )}
@@ -105,43 +97,64 @@ function SalonRecommendationView() {
         const handleSelect = async (address) => {
             setValue(address, false);
             clearSuggestions();
-
             const results = await getGeocode({ address });
             const { lat, lng } = await getLatLng(results[0]);
-            setSelected({ lat, lng });
+            setSelected({lat: lat, lng: lng });
+            searchNearbySalon(lat, lng);
         };
 
         return (
             <>
-                <InputGroup className="my-3">
-                    <Combobox onSelect={handleSelect}>
-                <ComboboxInput
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                    disabled={!ready}
-                    className="combobox-input"
-                    placeholder="Search an address"
-                />
-                <ComboboxPopover>
-                    <ComboboxList>
-                    {status === "OK" &&
-                        data.map(({ place_id, description }) => (
-                        <ComboboxOption key={place_id} value={description} />
-                        ))}
-                    </ComboboxList>
-                </ComboboxPopover>
-            </Combobox>
-                    {/* <Button variant="outline-primary" onClick={searchNearbySalon}>
-                        <FaSearch />
-                    </Button> */}
-                    <Button variant="outline-info" onClick={searchNearbySalon}>Use current location</Button>
-                </InputGroup>
+                <Row className="my-3">
+                    <Col xs={12} md={9} lg={10}>
+                        <Combobox onSelect={handleSelect}>
+                        <ComboboxInput
+                            value={value}
+                            onChange={(e) => setValue(e.target.value)}
+                            disabled={!ready}
+                            className="combobox-input"
+                            placeholder="Search an address"
+                        />
+                        <ComboboxPopover>
+                            <ComboboxList>
+                            {status === "OK" &&
+                                data.map(({ place_id, description }) => (
+                                <ComboboxOption key={place_id} value={description} />
+                                ))}
+                            </ComboboxList>
+                        </ComboboxPopover>
+                        </Combobox>
+                    </Col>
+                    <Col xs="auto" md="auto">
+                        <Button variant="outline-info" onClick={locateUserLocation}><IoMdLocate /> Use current location</Button>
+                    </Col>
+                </Row>
             </>
         );
     };
 
-    const searchNearbySalon = () => {
-        axios.get(`http://localhost:5001/salons?lat=${lat}&lng=${lng}`)
+    const locateUserLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+            function(position) {
+                let lat = position.coords.latitude;
+                let lng = position.coords.longitude
+                setSelected({lat: lat, lng: lng})
+                searchNearbySalon(lat, lng);
+                setAddress(`${position.coords.latitude}, ${position.coords.longitude}`)
+            },
+            function(error) {
+                // setGeoError(error);
+                console.error("Error Code = " + error.code + " - " + error.message);
+            }
+        );
+        }else {
+            alert("Geolocation is not supported")
+        }
+    }
+
+    const searchNearbySalon = (inputLat, inputLng) => {
+        axios.get(`http://localhost:5001/salons?lat=${inputLat}&lng=${inputLng}`)
         .then(response => {
             const data = response.data
             var responseCode = data.code
@@ -158,43 +171,16 @@ function SalonRecommendationView() {
     }
 
 	useEffect(() => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-            function(position) {
-                setGeolocation(position.coords);
-                setLat(position.coords.latitude);
-                setLng(position.coords.longitude);
-                setAddress(`${position.coords.latitude}, ${position.coords.longitude}`)
-            },
-            function(error) {
-                // setGeoError(error);
-                console.error("Error Code = " + error.code + " - " + error.message);
-            }
-        );
-        }else {
-            alert("Geolocation is not supported")
-        }
+        
 	}, [])
 
     return (
         <>
-        {/* <MapGL className="map-container" /> */}
-            {/* <Container>
-                <InputGroup className="my-3">
-                    <Form.Control
-                        placeholder="Enter a street address"
-                        aria-label="Recipient's username"
-                        aria-describedby="basic-addon2"
-                        value={inputAddress}
-                    />
-                    <Button variant="outline-primary" onClick={searchNearbySalon}>
-                        <FaSearch />
-                    </Button>
-                    <Button variant="outline-info" onClick={searchNearbySalon}>Use current location</Button>
-                </InputGroup>
-            </Container> */}
             <Container>
-            {/* <Places /> */}
+                <PlacesAutocomplete setSelected={setSelected} />
+            </Container>
+            <Container>
+         
             {isLoaded && <Map />}
             </Container>
             <Container fluid>
