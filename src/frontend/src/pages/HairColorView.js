44b1ced5be4258ddc62489babe929d23cdfb7936
Bill Song <songchao456@gmail.com>
2023-02-01 13:17:34 -0500

    change react-webcam to video

--- src/frontend/src/pages/HairColorView.js
+++ src/frontend/src/pages/HairColorView.js
@@ -1,106 +1,103 @@
 
 import React, { useEffect, useRef, useState } from "react"
 import io from "socket.io-client"
-import Webcam from "react-webcam";
 import { CirclePicker, MaterialPicker } from 'react-color';
-import * as cam from "@mediapipe/camera_utils";
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import {BsPlayCircle, BsStopCircle, BsUpload} from 'react-icons/bs'
 
 import "../css/HairColorView.css"
 
 const socket = io.connect('http://localhost:5001/test')
 function HairColorView() {
-    const webcamRef = useRef(null);
-    const outputRef = useRef(null); 
     const photoRef = useRef(null);
-    const [ hairColor, setHairColor ] = useState({    r: "241",
-    g: "112",
-    b: "19",
-    a: "1",})
+    const videoRef = useRef(null);
+    const [ hairColor, setHairColor ] = useState({r: "241",g: "112",b: "19",a: "1",})
     const { r, g, b, a } = hairColor;
-    const [ videoFeed, showVideoFeed] = useState(false);
-    // const { r, g, b, a } = hairColor;
-	useEffect(() => {
+    const [isShowVideo, setIsShowVideo] = useState(false);
+    const [localMediaStream, setLocalMediaStream] = useState(null);
+ 
+    const constraints = {
+        video: {
+            width: 300,
+            height: 300,
+            audio: false,
+        }
+    };
+
+    const startCam = () => {
+        setIsShowVideo(true);
         socket.on('connect', function() {
             console.log('Connected!');
         });
-        let camera;
-        if (
-            typeof webcamRef.current !== "undefined" &&
-            webcamRef.current !== null
-        ) {
-            camera = new cam.Camera(webcamRef.current.video, {
-                onFrame: () => {
-                    paintToCanvas(webcamRef.current.video);
-                },
-                width: 640,
-                height: 480,
-                
-            });
-            camera.start();
-        }
-        return () => {
-            // console.log('leaving!');
-            camera.stop();
-            camera = null;
-            showVideoFeed(false);
-        }   
-	}, [])
-
-  const paintToCanvas = (video) => {
-    if (!video){
-        return;
+        
+        navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+            videoRef.current.srcObject = stream;
+            setLocalMediaStream(stream)
+            let fps = 10;
+            setInterval(function () {
+                paintToCanvas();
+            }, 1000/fps);
+        }).catch(function(error) {
+            console.log(error);
+        });
     }
-    let photo = photoRef.current;
-    let ctx = photo.getContext("2d");
-    showVideoFeed(true);
-    const width = 320;
-    const height = 240;
-    photo.width = width;
-    photo.height = height;
-    ctx.drawImage(video, 0, 0, width, height);
-    let dataURL = photo.toDataURL('image/jpeg');
-    socket.emit('input image', dataURL);
-    // socket.on('out-image-event',function(data){
-    //     if (outputRef.current){
-    //         outputRef.current.setAttribute('src', data.image_data);
-    //         // outputRef.current.setAttribute('alt', 'outputref');
-    //     }
-    // });
-  };
 
-  return (
+    const stopCam = () => {
+        const tracks = localMediaStream.getTracks();
+        tracks.forEach(track => track.stop());
+        setIsShowVideo(false);
+    }
+
+    const paintToCanvas = () => {
+        let video = videoRef.current;
+        let photo = photoRef.current;
+        let ctx = photo.getContext("2d");
+
+        const width = 300;
+        const height = 300;
+        photo.width = width;
+        photo.height = height;
+        ctx.drawImage(video, 0, 0, width, height);
+        let dataURL = photo.toDataURL('image/jpeg');
+        socket.emit('input image', dataURL);
+    };
+
+    return (    
     <>
-    <Container fluid className="page-container">    
-        <Container fluid className="video-container">
-            <Webcam ref={webcamRef} hidden />
-            <canvas ref={photoRef}  />
-            {/* <img ref={outputRef} alt="transformed_output"></img> */}
-            {videoFeed && <img src="http://localhost:5001/video_feed"  alt="transformed_output"></img>}
+        <Container fluid className="page-container">    
+            <Container>
+                <Button className='mx-1' onClick={startCam}><BsPlayCircle /> Start Video Feed</Button>
+                <Button className='mx-1' onClick={stopCam}><BsStopCircle /> Stop Video Feed</Button>
+                <Button className='mx-1'><BsUpload /> Upload an Image</Button>
+            </Container>
+            <Container fluid className="video-container">
+                <Row>
+                    <Col>
+                        {isShowVideo &&(<video className="webcam-video" autoPlay={true} ref={videoRef}></video>)}
+                        {isShowVideo &&(<canvas ref={photoRef}/>)}
+                    </Col>
+                    <Col>
+                        {isShowVideo && <img src="http://localhost:5001/video_feed"  alt="transformed_output"></img>}
+                    </Col>
+                </Row>
+            </Container>
+            <Container fluid className="picker-container">
+                <Row lg={12}>
+                    <Col>
+                        <CirclePicker
+                            color = {hairColor}
+                            onChangeComplete={(color) => {
+                                setHairColor(color.rgb);
+                            }}
+                            className="color-picker"
+                        />
+                    </Col>
+                    <Col>
+                        <MaterialPicker color = {hairColor} className="color-picker"/>
+                    </Col>
+                </Row>
+            </Container>
         </Container>
-        <Container fluid className="picker-container">
-            <Row lg={12}>
-                <Col>
-            <CirclePicker
-                color = {hairColor}
-                onChangeComplete={(color) => {
-                    setHairColor(color.rgb);
-                }}
-                className="color-picker"
-            />
-            </Col>
-            {/* <Col></Col> */}
-            <Col>
-            <MaterialPicker color = {hairColor} className="color-picker"/>
-            </Col>
-            </Row>
-            {/* <Container>
-            <MaterialPicker color = {hairColor} className="color_picker"/>
-            </Container> */}
-            {/* <h1>Chosen Color</h1>
-            <h1>{`${r}, ${g}, ${b}, ${a}`}</h1> */}
-        </Container>
-    </Container>
     </>
   );
 }
