5eb16ac92de368aadb6816b14e3de6b9b99d159e
Hongwei Niu <niuh4@mcmaster.ca>
2023-04-01 20:41:42 -0700

    update workers code

--- src/backend/server/worker.py
+++ src/backend/server/worker.py
@@ -8,42 +8,60 @@ from collections import deque
 
 class Worker(object):
     def __init__(self, makeup_artist):
-        self.to_process = deque([])
-        self.to_output = deque([])
-        self.makeup_artist = makeup_artist
+        """
+        Initialize a Worker object with the specified makeup_artist.
 
+        Args:
+            makeup_artist (obj): An instance of a makeup_artist class responsible for applying the effects.
+        """
+        self.to_process = deque([]) # A deque for storing input frames to be processed
+        self.to_output = deque([]) # A deque for storing processed output frames
+        self.makeup_artist = makeup_artist # The makeup_artist instance responsible for applying effects
+
+        # Start a daemon thread to continuously process frames
         thread = threading.Thread(target=self.keep_processing, args=())
         thread.daemon = True
         thread.start()
 
     def process_one(self):
+        """
+        Process a single frame from the input deque and store the result in the output deque.
+        """
         if not self.to_process:
             return
 
         time_start = time()
-        # input is an ascii string.
+        # # Dequeue an input frame and its color from the input deque (input is an ascii string)
         input_str, color = self.to_process.popleft()
 
-        # convert it to a cv2 image
+        # Convert the input base64 string to a cv2 image
         input_img = base64_to_cv2_image(input_str)
 
-        # output_img = self.makeup_artist.apply_makeup(input_img)
+        # Apply the specified hair color to the input image
         output_img = self.makeup_artist.apply_hair_color(input_img, color)
 
-        # output_str is a base64 string in ascii
+        # Convert the output cv2 image to a base64 string
         output_str = cv2_image_to_base64(output_img)
 
-        # convert the base64 string in ascii to base64 string in _bytes_
+        # Convert the base64 string from ASCII to bytes and append it to the output deque
         self.to_output.append(binascii.a2b_base64(output_str))
-        # print("Lapsed time: {}, to_process: {}, to_output: {}".format(
-        #     time() - time_start, len(self.to_process), len(self.to_output)))
+        
 
     def keep_processing(self):
+        """
+        Continuously process input frames in a separate thread.
+        """
         while True:
             self.process_one()
             sleep(0)
 
     def enqueue_input(self, input):
+        """
+        Enqueue an input frame and its color into the input deque.
+
+        Args:
+            input (tuple): A tuple containing the input frame in base64 format and the color to apply.
+        """
         self.to_process.append(input)
 
     def get_frame(self):
