08bde94a78d3566e042f6d8889653ebd8a08f4cf
Hongwei Niu <niuh4@mcmaster.ca>
2023-04-01 20:43:29 -0700

    complete comments for workers.py

--- src/backend/server/worker.py
+++ src/backend/server/worker.py
@@ -65,11 +65,20 @@ class Worker(object):
         self.to_process.append(input)
 
     def get_frame(self):
+        """
+        Get the next processed output frame from the output deque.
+
+        Returns:
+            bytes: The processed output frame in byte format.
+        """
         while not self.to_output:
             sleep(0.01)
         return self.to_output.popleft()
 
     def clean_up(self):
+        """
+        Clear both input and output deques.
+        """
         self.to_process.clear()
         self.to_output.clear()
 
@@ -77,10 +86,32 @@ class Worker(object):
 class ImageWorker:
     
     def __init__(self, makeup_artist):
+        """
+        Initialize an ImageWorker object with the specified makeup_artist.
+
+        Args:
+            makeup_artist (obj): An instance of a makeup_artist class responsible for applying the effects.
+        """
         self.makeup_artist = makeup_artist
     
     def process_one(self, input_image, color):
+        """
+        Process a single input image and apply the specified hair color.
+
+        Args:
+            input_image (bytes): The input image in bytes format.
+            color (tuple): A tuple containing the RGB values of the hair color to apply.
+
+        Returns:
+            str: The processed output image in base64 string format.
+        """
+        # Convert the input image in bytes format to a cv2 image
         input_image = bytes_to_cv2_image(input_image)
+
+        # Apply the specified hair color to the input image
         output_img = self.makeup_artist.apply_hair_color(input_image, color)
+        
+        # Convert the output cv2 image to a base64 string
         output_str = cv2_image_to_base64(output_img)
+        
         return output_str
