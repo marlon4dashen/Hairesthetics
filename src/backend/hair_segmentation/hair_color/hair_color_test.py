3908c34667e2d9bfcb27ba9ba89206351bde3daa
Hongwei Niu <niuh4@mcmaster.ca>
2023-04-01 19:50:17 -0700

    backend code updates

--- src/backend/hair_segmentation/hair_color/hair_color_test.py
+++ src/backend/hair_segmentation/hair_color/hair_color_test.py
@@ -83,18 +83,31 @@ def test_change_color(test_image):
     assert np.all(output[10:30, 20:80, :] == [255, 0, 0])
 
 def test_change_hair_color(test_image):
+    """
+    Test the hair color changing functionality.
+    """
+
+    # Initialize the hair segmentation model
     hair_segmentation_model = 'path/to/model.pb'
     model = initialize_hair_segmentation_model(hair_segmentation_model)
+    
+    # Define model input and output parameters
     input_name = 'input'
     input_width, input_height = 100, 100
     output_names = ['output']
+
+    # Initialize the mediapipe face detection sub-module
     mpFaceDetection = initialize_mediapipe()
 
     target_color = [255, 0, 0]
 
+    # Change the hair color
     processed_frame = change_hair_color(
         test_image, target_color, model.session, input_name, input_width, input_height, output_names, mpFaceDetection)
 
+    # Check if the processed frame is a NumPy array and has the correct shape
     assert isinstance(processed_frame, np.ndarray)
     assert processed_frame.shape == test_image.shape
+
+    # Verify that the hair area in the processed frame has the target color
     assert np.all(processed_frame[10:30, 20:80, :] == [255, 0, 0])
