b0a75d2027e60aa5041d1d61858387c6e0545e3b
Hongwei Niu <niuh4@mcmaster.ca>
2023-04-01 21:13:00 -0700

    update utils.py

--- src/backend/utils/utils.py
+++ src/backend/utils/utils.py
@@ -3,8 +3,9 @@ from io import BytesIO
 import numpy
 import base64
 
-
+# Function to convert a given OpenCV (cv2) image to a base64-encoded string.
 def cv2_image_to_base64(cv_img):
+    # Convert the OpenCV image to a PIL image.
     im_pil = Image.fromarray(cv_img)
     buf = BytesIO()
     im_pil.save(buf, format="JPEG")
