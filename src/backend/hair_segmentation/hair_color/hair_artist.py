31e10e3ab326a45934dd62c00789f5e5478045c7
Hongwei Niu <niuh4@mcmaster.ca>
2023-04-01 19:54:34 -0700

    complete code comments for hair artist code

--- src/backend/hair_segmentation/hair_color/hair_artist.py
+++ src/backend/hair_segmentation/hair_color/hair_artist.py
@@ -19,10 +19,20 @@ class Hair_Artist(object):
         self.model = hair_color.initialize_hair_segmentation_model('./model/best_model_simplifier.onnx')
 
     def apply_makeup(self, img):
+        """
+        Apply makeup to the input image by converting it to grayscale.
+        """
+        # Convert the input image to grayscale
         return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        
+        # Alternative: Flip the input image horizontally
         # return img.transpose(Image.FLIP_LEFT_RIGHT)
 
     def apply_hair_color(self, img, color):
+        """
+        Apply the specified hair color to the input image using the hair segmentation model.
+        """
+        # Change the hair color in the input image using the initialized hair segmentation model and face detection module
         return hair_color.change_hair_color(img, color, self.model.session, 
             self.model.input_name, self.model.input_width, self.model.input_height, self.model.output_name, self.mpFace)
 
