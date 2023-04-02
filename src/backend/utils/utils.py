a195d62eae75a0a7a9c43c61d09ddb74d504549b
Hongwei Niu <niuh4@mcmaster.ca>
2023-04-01 21:13:50 -0700

    write code comments for utils.py

--- src/backend/utils/utils.py
+++ src/backend/utils/utils.py
@@ -13,9 +13,11 @@ def cv2_image_to_base64(cv_img):
 
     # Save the PIL image in JPEG format to the buffer.
     im_pil.save(buf, format="JPEG")
+
+    # Encode the buffer contents as a base64-encoded string and return it.
     return base64.b64encode(buf.getvalue())
 
-
+# Function to convert a given base64-encoded image string to an OpenCV (cv2) image.
 def base64_to_cv2_image(base64_img):
     img = Image.open(BytesIO(base64.b64decode(base64_img)))
     return numpy.array(img) 
