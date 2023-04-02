3bc7b4e47f1f43f8c1a5ed603a771bd5f1affc59
Hongwei Niu <niuh4@mcmaster.ca>
2023-04-01 19:53:45 -0700

    update hair artist code

--- src/backend/hair_segmentation/hair_color/hair_artist.py
+++ src/backend/hair_segmentation/hair_color/hair_artist.py
@@ -4,8 +4,18 @@ from model import model_utils
 import cv2
 
 class Hair_Artist(object):
+    """
+    Hair_Artist class for applying hair color changes to images.
+    """
+
     def __init__(self):
+        """
+        Initialize the Hair_Artist object with face detection and hair segmentation models.
+        """
+        # Initialize the Mediapipe face detection module
         self.mpFace = hair_color.initialize_mediapipe()
+        
+        # Initialize the hair segmentation model
         self.model = hair_color.initialize_hair_segmentation_model('./model/best_model_simplifier.onnx')
 
     def apply_makeup(self, img):
