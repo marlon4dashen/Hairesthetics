74e7ff0390d0f8ed2ac5f4bd80e1fe508bf300f5
Qiushi Xu <xuq37@mcmaster.ca>
2023-04-03 15:25:19 -0400

    update to Snapshot.js

--- src/frontend/src/pages/Snapshot.js
+++ src/frontend/src/pages/Snapshot.js
@@ -1,15 +1,19 @@
+// Declare four references to canvas elements using the useRef hook
 const faceFilterCanvasRef = useRef(null);
 const canvasRef = useRef(null);
 const pictureCanvasRef = useRef(null);
 const logoCanvasRef = useRef(null);
 
+// Define a function to capture a snapshot of the image with the filter applied
 const snapshot = useCallback(() => {
   const canvas = pictureCanvasRef.current;
   canvas.getContext('2d').drawImage(faceFilterCanvasRef.current, 0, 0);
 
+  // Create a new image element with the logo to be added to the snapshot
   const img = new Image();
   img.src = '/images/logo.png';
 
+  // Add event listeners to the image element to handle errors and loading
   img.addEventListener('error', e => {
     console.error(e);
   });
@@ -24,6 +28,7 @@ const snapshot = useCallback(() => {
         42.38
       );
 
+    // Use the mergeImages utility function to combine the filtered image, original image, and logo
     mergeImages([
       canvas.toDataURL('image/png'),
       canvasRef.current.toDataURL('image/png'),
